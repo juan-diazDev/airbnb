a6a87a9a7cb793f4edb2bbc320e7ef59e4730b83
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-15 07:51:53 -0500

    feat: add components for menu

---
+++ src/components/Menu/Index.jsx
@@ -0,0 +1,27 @@
+import './style.scss'
+
+function Menu() {
+  return (
+    <div class="menu--queri-bgscreen">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#0">
+                    <div class="menu__icon"></div>
+                    <div class="menu__icon"></div>
+                    <div class="menu__icon"></div>
+                  </a>
+                  <ul class="menu__item1">
+                    <li><a href="#0">Regístrate</a></li>
+                    <li><a href="#0">Inicia Sesión</a></li>
+                    <hr class="menu__horiLine" />
+                      <li><a href="#0">Contacto</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+  )
+}
+
+export default Menu
