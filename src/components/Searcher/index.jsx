8ee08c1680ef4913776a525d85ef6c6fccd34308
Juan Díaz <104471343+Dhruva108@users.noreply.github.com>
2022-07-16 14:19:53 -0500

    feat: correct word "className" in searcher file

--- src/components/Searcher/index.jsx
+++ src/components/Searcher/index.jsx
@@ -2,12 +2,12 @@ import './style.scss'
 
 function Searcher() {
   return (
-    <div class="searcher searcher--queri-bgscreen">
+    <div className="searcher searcher--queri-bgscreen">
       <form>
-        <button class="searcher__input searcher__input--queri-bgscreen"
+        <button className="searcher__input searcher__input--queri-bgscreen"
           type="submit">
-          <div class="container-image container-image--queri-bgscreen">
-            <img class="container-image__image container-image__image--queri-bgscreen"
+          <div className="container-image container-image--queri-bgscreen">
+            <img className="container-image__image container-image__image--queri-bgscreen"
               src="img/SearchIcons/Pink.png"
               alt="search-icon" />
           </div>
