f254c8396429828183e213d124b196d52ddb09cb
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-21 13:42:56 -0500

    feat: rename an importation in Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header'
-import MainSection from '../components/SpacesSection'
+import MainSection from '../components/MainSection'
 import Footer from '../components/Footer'
 
 export const Home = () => {
