dcb69d8c9e6e23657d855e54eeace6fcfaeb1be6
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-22 13:43:16 -0500

    feat: modify footer, app,jsx and add spaceDetail folder and file

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import {
   Route
 } from "react-router-dom"
 import Home from './pages/Home';
+// import Loginform from './components/Log-In-form/'
+// import Signupform from './components/Sign-up-form/'
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/" element={ <Home /> }/>
+      {/* <Route path="/Loginform" element={ <Loginform /> }/>
+      <Route path="/Signupform" element={ <Signupform/> }/> */}
     </Routes>
    </BrowserRouter>
   );
---
+++ src/pages/SpaceDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpaceDetail = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SpaceDetail
