b6af0d65d9eb7311474044204dd6b9ca99ab2a67
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-24 13:41:19 -0500

    feat: creat services for .json

--- src/pages/SpaceDetail.jsx
+++ src/pages/SpaceDetail.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import CheckOutCard from '../components/CheckOutCard'
+import Header from "../components/Header"
 
 const SpaceDetail = () => {
   return (
     <div>
-
+      <Header />
+      <CheckOutCard />
     </div>
   )
 }
