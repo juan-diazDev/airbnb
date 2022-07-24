adaba76be82857aebea65af47a9f5c0945b33627
Jessica Martinez Alee <105444324+Jessaleem@users.noreply.github.com>
2022-07-24 15:37:50 -0500

    [feat]:add link and styles to backbutton (#28)
    feat:add link and styles to backbutton
    
    Co-authored-by: Juan Díaz <104471343+Dhruva108@users.noreply.github.com>

--- src/components/Log-In-form/index.jsx
+++ src/components/Log-In-form/index.jsx
@@ -1,32 +1,35 @@
 import './styles.scss'
+import {Link} from "react-router-dom"
 
 function Loginform() {
 
 
 return (
 
-    <section>
-        <div className="Login__container">
-            <div className="Login">
-            <header className="Login_headerbox">
-                <button className="Login__backbutton">
-                    <span>˂</span>  
-                </button>
-                <h1>Log In or sign up</h1>
-            </header>
-            <form action="" className="Login__form">
-                <p className="Login__welcome">Welcome to Airbnb</p>
-                <input className="Login__textbox" type="email" name="email" placeholder="Email" />
-                <input className="Login__textbox" type="password" name="password" placeholder="Password" />
-                <button className="Login__btn__continue"><b>Continue</b></button>
-                <div className="Login__divisor">
-                    <p>or</p>
-                </div>
-                <button className="Login__btn__Signin"><b>Sign up</b></button>
-            </form>
-            </div>
-        </div>
-    </section>
+  <section>
+    <div className="Login__container">
+      <div className="Login">
+      <header className="Login_headerbox">
+          <Link to = "/">
+          <button className="Login__backbutton">
+              <span>˂</span>  
+          </button>
+          </Link>
+          <h1>Log In or sign up</h1>
+      </header>
+      <form action="" className="Login__form">
+          <p className="Login__welcome">Welcome to Airbnb</p>
+          <input className="Login__textbox" type="email" name="email" placeholder="Email" />
+          <input className="Login__textbox" type="password" name="password" placeholder="Password" />
+          <button className="Login__btn__continue"><b>Continue</b></button>
+          <div className="Login__divisor">
+              <p>or</p>
+          </div>
+          <button className="Login__btn__Signin"><b>Sign up</b></button>
+      </form>
+      </div>
+    </div>
+  </section>
 
 
 )
