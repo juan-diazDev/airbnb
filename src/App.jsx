a9da8fed087975179906f1c4bd877bb5ae267183
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-25 11:43:20 -0500

    feat: add become-a-host bbutton

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import Loginform from './components/Log-In-form/';
 import Signupform from './components/Sign-up-form/';
 import SpaceDetail from './pages/SpaceDetail';
+import FormContainerType from './components/FormContainerType'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Route path="/Loginform" element={ <Loginform /> }/>
       <Route path="/Signupform" element={ <Signupform/> }/>
       <Route path='/SpaceDetail' element={<SpaceDetail /> }/>
+      <Route path='/FormContainerType' element={<FormContainerType />} />
     </Routes>
    </BrowserRouter>
   );
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,23 +3,27 @@ import { Link } from "react-router-dom"
 
 function Menu() {
   return (
-    <div className="menu--queri-bgscreen">
-      <ul className='nav-menu'>
-        <li>
-          <div className='menu__icon-container'>
-            <div className="menu__icon"></div>
-            <div className="menu__icon"></div>
-            <div className="menu__icon"></div>
-          </div>
-          <ul className="menu__item">
-            <li><Link to="/Signupform" >Sing up</Link></li>
-            <li><Link to="/Loginform" >Log in</Link></li>
-            <hr className="menu__line" />
-            <li><Link to="/" >Contact</Link></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className='menu__hosting-link'><Link to="/FormContainerType"> <strong>Become a host</strong></Link></div>
+      <div className="menu--queri-bgscreen">
+        <ul className='nav-menu'>
+          <li>
+            <div className='menu__icon-container'>
+              <div className="menu__icon"></div>
+              <div className="menu__icon"></div>
+              <div className="menu__icon"></div>
+            </div>
+            <ul className="menu__item">
+              <li><Link to="/Signupform" >Sing up</Link></li>
+              <li><Link to="/Loginform" >Log in</Link></li>
+              <li><Link to="/FormContainerType" >Host your home</Link></li>
+              <hr className="menu__line" />
+              <li><Link to="/" >Contact</Link></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
