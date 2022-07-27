2ab164bf35b87a37c48d9d36ce788f242f5a87a9
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2022-07-27 13:08:29 -0500

    [feat]: Add Eslint and Husky to the project (#43)
    feat: Add Eslint and Husky to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Loginform from './components/Log-In-form/';
-import Signupform from './components/Sign-up-form/';
+import Loginform from './components/Log-In-form';
+import Signupform from './components/Sign-up-form';
 import SpaceDetail from './pages/SpaceDetail';
-import FormContainerDescribe from './components/FormContainerDescribe/index.jsx';
-import FormContainerCapacity from './components/FormContainerCapacity/index.jsx';
-import FormContainerAdress from './components/FormContainerAdress/index.jsx';
-import FormContainerFeatures from './components/FormContainerFeatures/index.jsx';
-import FormContainerAdditions from './components/FormContainerAdditions/index.jsx';
-import FormContainerImages from './components/FormContainerImages/index.jsx';
-import FormContainerTitle from './components/FormContainerTitle/index.jsx';
-import FormContainerDescriptionPlace from './components/FormContainerDescriptionPlace/index.jsx';
-import FormContainerPrice from './components/FormContainerPrice/index.jsx';
-import FormContainerType from './components/FormContainerType'
-
+import FormContainerDescribe from './components/FormContainerDescribe';
+import FormContainerCapacity from './components/FormContainerCapacity';
+import FormContainerAdress from './components/FormContainerAdress';
+import FormContainerFeatures from './components/FormContainerFeatures';
+import FormContainerAdditions from './components/FormContainerAdditions';
+import FormContainerImages from './components/FormContainerImages';
+import FormContainerTitle from './components/FormContainerTitle';
+import FormContainerDescriptionPlace from './components/FormContainerDescriptionPlace';
+import FormContainerPrice from './components/FormContainerPrice';
+import FormContainerType from './components/FormContainerType';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/Loginform" element={<Loginform />}/>
-      <Route path="/Signupform" element={<Signupform/>}/>
-      <Route path="/SpaceDetail" element={<SpaceDetail />}/>
-      <Route path="/FormContainerDescribe" element= {<FormContainerDescribe/>}/>
-      <Route path="/FormContainerCapacity" element={<FormContainerCapacity/>}/>
-      <Route path="/FormContainerAdress" element={<FormContainerAdress/>}/>
-      <Route path="/FormContainerFeatures" element={<FormContainerFeatures/>}/>
-      <Route path="/FormContainerAdditions" element={<FormContainerAdditions/>}/>
-      <Route path="/FormContainerImages" element={<FormContainerImages/>}/>
-      <Route path="/FormContainerTitle" element={<FormContainerTitle/>}/>
-      <Route path="/FormContainerDescriptionPlace" element={<FormContainerDescriptionPlace/>}/>
-      <Route path="/FormContainerPrice" element={<FormContainerPrice/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/Loginform" element={<Loginform />} />
+      <Route path="/Signupform" element={<Signupform />} />
+      <Route path="/SpaceDetail" element={<SpaceDetail />} />
+      <Route path="/FormContainerDescribe" element={<FormContainerDescribe />} />
+      <Route path="/FormContainerCapacity" element={<FormContainerCapacity />} />
+      <Route path="/FormContainerAdress" element={<FormContainerAdress />} />
+      <Route path="/FormContainerFeatures" element={<FormContainerFeatures />} />
+      <Route path="/FormContainerAdditions" element={<FormContainerAdditions />} />
+      <Route path="/FormContainerImages" element={<FormContainerImages />} />
+      <Route path="/FormContainerTitle" element={<FormContainerTitle />} />
+      <Route path="/FormContainerDescriptionPlace" element={<FormContainerDescriptionPlace />} />
+      <Route path="/FormContainerPrice" element={<FormContainerPrice />} />
       <Route path="/FormContainerType" element={<FormContainerType />} />
     </Routes>
   </BrowserRouter>
