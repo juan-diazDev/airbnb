f9d113abc7f62a110be8627a4433b067aba77852
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-26 23:18:12 -0500

    feat: fix link color in header an main-section

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Menu = () => (
   <>
-    <div className="menu__hosting-link"><Link to="/FormContainerType"> <strong>Become a host</strong></Link></div>
+    <div className="menu__hosting-link"><Link to="/FormContainerType" className="menu__item__link"> <strong>Become a host</strong></Link></div>
     <div className="menu--queri-bgscreen">
       <ul className="nav-menu">
         <li>
@@ -13,11 +13,11 @@ const Menu = () => (
             <div className="menu__icon"></div>
           </div>
           <ul className="menu__item">
-            <li><Link to="/Signupform" >Sing up</Link></li>
-            <li><Link to="/Loginform" >Log in</Link></li>
-            <li><Link to="/FormContainerType" >Host your home</Link></li>
+            <li><Link to="/Signupform" className="menu__item__link">Sing up</Link></li>
+            <li><Link to="/Loginform" className="menu__item__link">Log in</Link></li>
+            <li><Link to="/FormContainerType" className="menu__item__link">Host your home</Link></li>
             <hr className="menu__line" />
-            <li><Link to="/" >Contact</Link></li>
+            <li><Link to="/" className="menu__item__link">Contact</Link></li>
           </ul>
         </li>
       </ul>
