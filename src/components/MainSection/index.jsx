4cef32b50a5b2b2b7a12d0719a8fd16e0e49544a
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-07-27 23:25:14 -0500

    feat: modify and finish styles for new component

--- src/components/MainSection/index.jsx
+++ src/components/MainSection/index.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 import CardSpace from '../CardSpace';
+import './style.scss';
 
 const MainSection = ({ list }) => (
-  <>
+  <div className="cards-conatiner">
     {
       list.map((item) => (
         <CardSpace key={item.id} space={item} />
       ))
     }
-  </>
+  </div>
 );
 
 MainSection.propTypes = {
