d175447a5296dbf01a5bdb1ea1e345bce7a0d721
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-21 09:51:22 -0500

    feat: fix ruoute and img for verifyAccount

--- src/pages/VerifyAccount/index.jsx
+++ src/pages/VerifyAccount/index.jsx
@@ -4,7 +4,7 @@ const AccountVerfication = () => (
   <div className="account-verification__main-container">
     <div className="account-verification__message-container">
       <div className="account-verification__image-container">
-        <img src="https://res.cloudinary.com/ebquipo-maravilla/image/upload/v1659662767/images/AlterLogo/AlterLogo_-_pink2_v3pefc.png" alt="Logo" />
+        <img src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1659662767/images/AlterLogo/AlterLogo_-_pink2_v3pefc.png" alt="Logo" />
       </div>
       <h3>Your account is active!</h3>
     </div>
