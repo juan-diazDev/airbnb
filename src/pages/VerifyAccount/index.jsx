d3e4058eb27822f71a49bd549f74d65cd8264909
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-21 12:34:07 -0500

    feat: implement sendgrid(has bug)

--- src/pages/VerifyAccount/index.jsx
+++ src/pages/VerifyAccount/index.jsx
@@ -1,14 +1,34 @@
 import './styles.scss';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
-const AccountVerfication = () => (
-  <div className="account-verification__main-container">
-    <div className="account-verification__message-container">
-      <div className="account-verification__image-container">
-        <img src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1659662767/images/AlterLogo/AlterLogo_-_pink2_v3pefc.png" alt="Logo" />
+import { verify } from '../../services/auth';
+
+const AccountVerfication = () => {
+  const { token } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyAccount = async () => {
+      const data = await verify(token);
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('profile', JSON.stringify(data.profile));
+      navigate('/');
+    };
+
+    verifyAccount();
+  });
+
+  return (
+    <div className="account-verification__main-container">
+      <div className="account-verification__message-container">
+        <div className="account-verification__image-container">
+          <img src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1659662767/images/AlterLogo/AlterLogo_-_pink2_v3pefc.png" alt="Logo" />
+        </div>
+        <h3>Your account is active!</h3>
       </div>
-      <h3>Your account is active!</h3>
     </div>
-  </div>
-);
+  );
+};
 
 export default AccountVerfication;
