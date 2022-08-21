b6e12037e71f5c449970a26bd396dd353060c69d
Jessica <aleejess@gmail.com>
2022-08-20 22:01:56 -0500

    feat: import useState and signup

--- src/components/Sign-up-form/index.jsx
+++ src/components/Sign-up-form/index.jsx
@@ -1,7 +1,13 @@
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { singup } from '../../services/auth';
 
 const Signupform = () => {
+  const [signup, setSignup] = useState({});
+  const handleChange = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+  };
   const handlerSubmit = (e) => {
     e.preventDefault();
   };
@@ -20,10 +26,10 @@ const Signupform = () => {
             <h2>Sign up</h2>
           </header>
           <form action="" className="signup__form" onSubmit={handlerSubmit}>
-            <input className="signup__namebox" type="text" name="names" placeholder="First name" />
-            <input className="signup__lastnamebox" type="text" name="lastname" placeholder="Last name" />
+            <input className="signup__namebox" type="text" name="name" placeholder="First name" />
+            <input className="signup__lastnamebox" type="text" name="lastName" placeholder="Last name" />
             <p className="signup__helptext">Make sure it matches the name on your government ID.</p>
-            <input className="signup__textbox" type="date" name="birthdate" placeholder="Birthdate" />
+            <input className="signup__textbox" type="date" name="birthDate" placeholder="Birthdate" />
             <p className="signup__helptext">To sign up, you need to be at least 18. Your birthday won’t be shared with other people who use Airbnb.</p>
             <input className="signup__textbox" type="email" name="email" placeholder="Email" />
             <p className="signup__helptext">We’ll email you trip confirmations and receipts.</p>
@@ -39,7 +45,7 @@ const Signupform = () => {
               <option value="host">Host</option>
             </select>
             <p className="signup__helptext">Choose your role (Guest/Host).</p>
-            <select className="signup__textboxSelect" placeholder="Gender" defaultValue="">
+            <select className="signup__textboxSelect" name="gender" placeholder="Gender" defaultValue="">
               <option value="" disabled>Gender</option>
               <option value="other">Other</option>
               <option value="male">Male</option>
@@ -49,7 +55,7 @@ const Signupform = () => {
             <p className="signup__policies1">By selecting Agree and continue, I agree to Airbnb’s <span className="signup__policies2" target="blank">Terms of Service</span>,
               <span className="signup__policies2" target="blank"> Payments Terms of Service</span>, <span className="signup__policies2" target="blank"> Nondiscrimination Policy</span> and <span className="signup__policies2" target="blank">Colombia Privacy Supplement</span>, and acknowledge the <span className="signup__policies2" target="blank">Privacy Policy.</span>
             </p>
-            <button className="signup__btn" type="button"><b>Agree and continue</b></button>
+            <button className="signup__btn" type="submit"><b>Agree and continue</b></button>
             <p className="signup__final">Airbnb will send you members-only deals, inspiration, marketing emails, and push notifications. You can opt out of receiving these at any time in your account settings or directly from the marketing notification.
             </p>
             <div className="signup__checkbox">
