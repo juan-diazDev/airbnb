7a4f1a12b76b3ac9c569f4bad535e44110bcf7bc
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-24 23:46:15 -0500

    feat: delete console.log from cardSpace

--- src/components/CardSpace/index.jsx
+++ src/components/CardSpace/index.jsx
@@ -3,25 +3,22 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-const CardSpace = ({ space }) => {
-  console.log('🚀', space);
-  return (
-    <main className="mainSection mainSection--queri-bgscreen">
-      <Link className="space-link" to={`/SpaceDetail/${space._id}`} key={space._id}>
-        <div className="card__image-container">
-          <div>
-            <img className="card__image__image" src={space.img} alt={space.title} />
-          </div>
+const CardSpace = ({ space }) => (
+  <main className="mainSection mainSection--queri-bgscreen">
+    <Link className="space-link" to={`/SpaceDetail/${space._id}`} key={space._id}>
+      <div className="card__image-container">
+        <div>
+          <img className="card__image__image" src={space.img} alt={space.title} />
         </div>
-      </Link>
-      <div className="info-container">
-        <div className="card__title"><b>{space.title}</b></div>
-        <p className="card__description">{space.description}</p>
-        <div className="card__price"> ${space.price} <span>night</span></div>
       </div>
-    </main>
-  );
-};
+    </Link>
+    <div className="info-container">
+      <div className="card__title"><b>{space.title}</b></div>
+      <p className="card__description">{space.description}</p>
+      <div className="card__price"> ${space.price} <span>night</span></div>
+    </div>
+  </main>
+);
 
 CardSpace.propTypes = {
   space: PropTypes.shape().isRequired,
