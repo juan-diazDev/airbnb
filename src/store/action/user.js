6e0c2a2214c1a398a1d8ace998f67a7a78db66af
Jessica <aleejess@gmail.com>
2022-08-24 19:16:50 -0500

    feat: update store, action user and constant user

--- src/store/action/user.js
+++ src/store/action/user.js
@@ -1,10 +1,10 @@
-import { LOAD_USER_LIST, SET_USER_DETAIL, SET_ERROR } from '../constants/user';
+import { LOAD_USER, SET_USER_DETAIL, SET_ERROR } from '../constants/user';
 
 import { getUsers } from '../../services/users';
 
 const setError = (error) => ({ type: SET_ERROR, payload: error });
 
-export const loadUserList = (userList) => ({ type: LOAD_USER_LIST, payload: userList });
+export const loadUserList = (userList) => ({ type: LOAD_USER, payload: userList });
 export const setUserDetail = (userDetail) => ({ type: SET_USER_DETAIL, payload: userDetail });
 
 export const fetchuserList = () => async (dispatch) => {
