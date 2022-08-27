6750fe5bfed18c2dacf9a3a9bd0aa3bb89084db0
Jessica <aleejess@gmail.com>
2022-08-27 00:10:04 -0500

    feat:consume information in spaces from Redux

--- src/store/action/space.js
+++ src/store/action/space.js
@@ -1,4 +1,15 @@
+import { getSpaces } from '../../services/spaces';
 import { LOAD_SPACE_LIST, SET_SPACE_DETAIL } from '../constants/space';
+import setError from './ui';
 
 export const loadSpaceList = (spaceList) => ({ type: LOAD_SPACE_LIST, payload: spaceList });
 export const loadSpaceDetail = (spaceDetail) => ({ type: SET_SPACE_DETAIL, payload: spaceDetail });
+
+export const fetchSpaceList = () => async (dispatch) => {
+  try {
+    const spaceDetail = await getSpaces();
+    dispatch(loadSpaceList(spaceDetail));
+  } catch (error) {
+    dispatch(setError(error));
+  }
+};
