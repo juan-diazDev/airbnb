93babde616361fae9930c24a23499fea394582dc
Jessica <aleejess@gmail.com>
2022-08-29 13:51:46 -0500

    feat: update info with Redux in Description in Detail Page

--- src/components/SpaceDetail/Description/index.jsx
+++ src/components/SpaceDetail/Description/index.jsx
@@ -14,7 +14,7 @@ const Description = ({ details }) => (
         <div className="description__host__privacity-type__icon-container">
           <img className="description__host__privacity-type__icon" src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1660015542/images/Amenities/PropertyTypeGroup/Ranch_cqiayn.png" alt="Ranch" />
         </div>
-        <p className="description__host__privacity-type__text">Entire place</p>
+        <p className="description__host__privacity-type__text">{details.privacyType}</p>
       </div>
     </section>
 
@@ -22,8 +22,8 @@ const Description = ({ details }) => (
 
     <section className="description__host__info-container">
       <div className="description__host__info__text-container">
-        <h3 className="host__info__text__name">Place Hosted by Jess</h3>
-        <p className="host__info__text__amenities">5 guests · 3 bedrooms · 5 beds · 3 bathroom</p>
+        <h3 className="host__info__text__name">Place Hosted by {details.host.name}</h3>
+        <p className="host__info__text__amenities">5 guests · 3 bedrooms · {details.amenities.beds} beds · 3 bathroom</p>
       </div>
       <div className="description__host__info__imgae-container">
         <img className="description__host__info__image" src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1659716573/images/Account/profilepic_bo57dk.jpg" alt="hosts-profile" />
