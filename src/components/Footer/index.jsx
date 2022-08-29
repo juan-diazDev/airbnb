984c7aae3d05072d8e28afcfea678d280b2717f9
Juan Díaz <104471343+Dhruva108@users.noreply.github.com>
2022-08-29 14:42:49 -0500

    [feat]: add conditionals showed info when logged in (#65)
    feat: add conditionals for info to show whrn logged in

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,34 +1,69 @@
 import './style.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const Footer = () => (
-  <footer className="footer">
-    <div className="footer__call-to-action-conatiner">
-      <Link to="/Loginform">
-        <button type="button" className="footer__call-to-action">
-          <strong>Log-in</strong>
-        </button>
-      </Link>
-    </div>
-    <div className="footer__bg-screen-container">
-      <div className="footer__left-container">
-        <div>© 2022, Equipo Maravilla</div>
-        <div>
-          <Link to="/Privacity">
-            Privacity
-          </Link>
-        </div>
+import AccountNavbar from '../AccountPage/AccountNavbar';
+
+const Footer = () => {
+  const navigate = useNavigate();
+
+  const result = localStorage.getItem('profile');
+  const profile = JSON.parse(result);
+
+  const handleClick = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+  return (
+    <footer className="footer">
+      <div className="footer__call-to-action-conatiner">
+        {
+          profile
+            ? <AccountNavbar />
+            : (
+              <Link to="/Loginform">
+                <button type="button" className="footer__call-to-action">
+                  <strong>Log-in</strong>
+                </button>
+              </Link>
+            )
+        }
       </div>
-      <div className="footer__right-container">
-        <div>
-          <Link to="/Loginform">Log-in</Link>
+      <div className="footer__bg-screen-container">
+        <div className="footer__left-container">
+          <div>© 2022, Equipo Maravilla</div>
+          <div>
+            <Link to="/Privacity">
+              Privacity
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link to="/Signupform">Sing-up</Link>
-        </div>
+        {
+          profile
+            ? (
+              <div className="footer__right-container">
+                <div>
+                  <Link to="/" onClick={handleClick}>Log-out</Link>
+                </div>
+                <div>
+                  <Link to="/Account">Profile</Link>
+                </div>
+              </div>
+            )
+            : (
+              <div className="footer__right-container">
+                <div>
+                  <Link to="/Loginform">Log-in</Link>
+                </div>
+                <div>
+                  <Link to="/Signupform">Sing-up</Link>
+                </div>
+              </div>
+            )
+        }
+
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
