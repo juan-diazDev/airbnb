ed8e21f62b7898a1db59e79699a61e25639281c9
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-29 12:37:50 -0500

    feat: add conditionals for info to show whrn logged in

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,25 +1,50 @@
 import './styles.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const Menu = () => (
-  <div className="menu--queri-bgscreen">
-    <ul className="nav-menu">
-      <li>
-        <div className="menu__icon-container">
-          <div className="menu__icon" />
-          <div className="menu__icon" />
-          <div className="menu__icon" />
-        </div>
-        <ul className="menu__item">
-          <li><Link to="/Signupform" className="menu__item__link">Sing up</Link></li>
-          <li><Link to="/Loginform" className="menu__item__link">Log in</Link></li>
-          <li><Link to="/PropertyTypeGroup" className="menu__item__link">Host your home</Link></li>
-          <hr className="menu__line" />
-          <li><Link to="/Account" className="menu__item__link">Contact</Link></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
-);
+const Menu = () => {
+  const navigate = useNavigate();
+
+  const result = localStorage.getItem('profile');
+  const profile = JSON.parse(result);
+
+  const handleClick = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+  return (
+    <div className="menu--queri-bgscreen">
+      <ul className="nav-menu">
+        <li>
+          <div className="menu__icon-container">
+            <div className="menu__icon" />
+            <div className="menu__icon" />
+            <div className="menu__icon" />
+          </div>
+          {
+            profile
+              ? (
+                <ul className="menu__item">
+                  <li><Link to="/Account" className="menu__item__link" aria-disabled>Hello, {profile.name}</Link></li>
+                  <li><Link to="/" className="menu__item__link" onClick={handleClick}>Log out</Link></li>
+                  <li><Link to="/PropertyTypeGroup" className="menu__item__link">Your reservation</Link></li>
+                  <hr className="menu__line" />
+                  <li><Link to="/Account" className="menu__item__link">Contact</Link></li>
+                </ul>
+              )
+              : (
+                <ul className="menu__item">
+                  <li><Link to="/Signupform" className="menu__item__link">Sing up</Link></li>
+                  <li><Link to="/Loginform" className="menu__item__link">Log in</Link></li>
+                  <li><Link to="/PropertyTypeGroup" className="menu__item__link">Host your home</Link></li>
+                  <hr className="menu__line" />
+                  <li><Link to="/Account" className="menu__item__link">Contact</Link></li>
+                </ul>
+              )
+          }
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Menu;
