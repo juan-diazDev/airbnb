c92307515abc3c7cd9d6911f250dc9f91a876548
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-29 21:51:06 -0500

    feat: modufy Header Index and make it a conditional

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,10 @@
-/* eslint-disable react/style-prop-object */
-import { Link, useLocation } from 'react-router-dom';
-import Menu from '../Menu';
-import Searcher from '../Searcher';
+import Hero from './Hero';
+import NavMenu from './NavMenu';
+
 import './style.scss';
 
-const Header = () => {
-  const { pathname } = useLocation();
-
-  return (
-    <header className={`${pathname.slice(1)} header header--bgscreen`}>
-      <div className="container-logo">
-        <Link to="/">
-          <img src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1659662807/images/Logo/Pink_lmr4oj.png" alt="airbnb-logo" className="container-logo__logo" />
-        </Link>
-      </div>
-      <Searcher />
-      <Menu />
-      <div className="header__hero__text">
-        <h1>Get-out and stretch your imagination</h1>
-      </div>
-    </header>
-  );
-};
+const Header = () => (window.location.pathname === '/'
+  ? <Hero />
+  : <NavMenu />);
 
 export default Header;
