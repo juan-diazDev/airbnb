81903545d435b617b4d75872856576c51b89eba0
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-08-31 14:54:26 -0500

    feat: add searcher

--- src/components/CardSpace/index.jsx
+++ src/components/CardSpace/index.jsx
@@ -5,19 +5,28 @@ import './style.scss';
 
 const CardSpace = ({ space }) => (
   <main className="mainSection mainSection--queri-bgscreen">
-    <Link className="space-link" to={`/SpaceDetail/${space._id}`} key={space._id}>
-      <div className="card__image-container">
-        <div>
-          <img className="card__image__image" src={space.img} alt={space.title} />
-        </div>
-      </div>
-    </Link>
-    <div className="info-container">
-      <div className="card__title"><b>{space.title}</b></div>
-      <p className="card__description">{space.description}</p>
-      <div className="card__price"> ${space.price} <span>night</span></div>
-    </div>
+    {
+      space
+        ? (
+          <>
+            <Link className="space-link" to={`/SpaceDetail/${space._id}`} key={space._id}>
+              <div className="card__image-container">
+                <div>
+                  <img className="card__image__image" src={space.img} alt={space.title} />
+                </div>
+              </div>
+            </Link>
+            <div className="info-container">
+              <div className="card__title"><b>{space.title}</b></div>
+              <p className="card__description">{space.description}</p>
+              <div className="card__price"> ${space.price} <span>night</span></div>
+            </div>
+          </>
+        )
+        : <div className="loader" />
+    }
   </main>
+
 );
 
 CardSpace.propTypes = {
