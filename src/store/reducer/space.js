c4c663b17198f533fc9aae226a72fb854ac13521
Juan Díaz <104471343+Dhruva108@users.noreply.github.com>
2022-08-31 01:45:29 -0500

    delete unnecesary white space at the beginning

--- src/store/reducer/space.js
+++ src/store/reducer/space.js
@@ -1,6 +1,5 @@
 import { LOAD_SPACE_LIST, SET_SPACE_REGISTER, SET_SPACE_DETAIL } from '../constants/space';
 
-
 function spaceReducer(state = {}, action = {}) {
   switch (action.type) {
     case LOAD_SPACE_LIST:
