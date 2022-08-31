224117679680abd0c39dabc0faabe1b44e86f701
Juan Díaz <104471343+Dhruva108@users.noreply.github.com>
2022-08-31 12:15:07 -0500

    [Feat]: fix view in spaceDetail and .env for BASE_URL (#71)
    * feat/change BASE_URL from local to Heroku
    
    * feat: add conditions for render in spaceDetail
    
    * feat: fix position in spaceDetail

--- src/components/SpaceDetail/Description/index.jsx
+++ src/components/SpaceDetail/Description/index.jsx
@@ -7,14 +7,14 @@ const Description = ({ details }) => (
         <div className="description__host__type__icon-container">
           <img className="description__host__type__icon" src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1660015542/images/Amenities/PropertyTypeGroup/Ranch_cqiayn.png" alt="Ranch" />
         </div>
-        <p className="description__host__type__text">{details.type}</p>
+        <p className="description__host__type__text">{details?.type}</p>
       </div>
 
       <div className="description__host__privacity-type-container">
         <div className="description__host__privacity-type__icon-container">
           <img className="description__host__privacity-type__icon" src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1660015542/images/Amenities/PropertyTypeGroup/Ranch_cqiayn.png" alt="Ranch" />
         </div>
-        <p className="description__host__privacity-type__text">{details.privacyType}</p>
+        <p className="description__host__privacity-type__text">{details?.privacyType}</p>
       </div>
     </section>
 
@@ -22,8 +22,8 @@ const Description = ({ details }) => (
 
     <section className="description__host__info-container">
       <div className="description__host__info__text-container">
-        <h3 className="host__info__text__name">Place Hosted by {details.host.name}</h3>
-        <p className="host__info__text__amenities">5 guests · 3 bedrooms · {details.amenities.beds} beds · 3 bathroom</p>
+        <h3 className="host__info__text__name">Place Hosted by {details?.host?.name}</h3>
+        <p className="host__info__text__amenities">5 guests · 3 bedrooms · {details?.amenities?.beds} beds · 3 bathroom</p>
       </div>
       <div className="description__host__info__imgae-container">
         <img className="description__host__info__image" src="https://res.cloudinary.com/equipo-maravilla/image/upload/v1659716573/images/Account/profilepic_bo57dk.jpg" alt="hosts-profile" />
@@ -33,7 +33,7 @@ const Description = ({ details }) => (
     <hr className="spaceDetail__line-separation" />
 
     <section className="description__space__description-container">
-      <p className="description__space__description-text"> {details.description}
+      <p className="description__space__description-text"> {details?.description}
       </p>
     </section>
 
