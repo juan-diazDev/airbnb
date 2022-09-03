f75e686e49c9efe39672c40deef8c99388db3d50
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-03 18:14:47 -0500

    feat: finish info section of payment page

--- src/components/Payments/index.jsx
+++ src/components/Payments/index.jsx
@@ -1,8 +1,20 @@
-/* eslint-disable no-console */
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useSelector } from 'react-redux';
+import swal from 'sweetalert';
 import './styles.scss';
 
 const Payments = () => {
+  const { checkoutForm } = useSelector((state) => state.checkout);
+  const {
+    adults,
+    checkOut,
+    checkIn,
+    children,
+    pets,
+    price,
+    image,
+    title,
+  } = checkoutForm;
   const stripe = useStripe();
   const elements = useElements();
 
@@ -15,11 +27,14 @@ const Payments = () => {
     });
 
     if (error) {
-      console.log({ '[Error]': error.message });
+      swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: error.message,
+      });
       return;
     }
 
-    console.log({ '[Paymethod]': paymentMethod });
     const token = localStorage.getItem('token');
     const options = {
       method: 'POST',
@@ -29,21 +44,67 @@ const Payments = () => {
       },
       body: JSON.stringify({
         paymentMethod,
-        amount: 10_000,
+        amount: price,
       }),
     };
 
     const response = await fetch('http://localhost:3030/api/payment', options);
-    console.log('🚀 ~ file: index.jsx ~ line 37 ~ handleSubmit ~ response', response);
-    // const body = await response.json();
+    if (response) {
+      swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success',
+      );
+    } else {
+      swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+      });
+    }
   };
 
   return (
-    <div className="background">
-      <form onSubmit={handleSubmit}>
-        <CardElement />
-        <button type="submit">Submit</button>
-      </form>
+    <div className="payment__main-container">
+      <section className="payment__left-container">
+        <div className="payment__info-conatiner">
+          <h1>{title}</h1>
+          <div className="payment__image-conatiner">
+            <img className="payment__image__image" src={image} alt="space" />
+          </div>
+          <div className="payment__adults-info">
+            <p>Adult</p>
+            <div>{adults}</div>
+          </div>
+          <div className="payment__children-info">
+            <p>children</p>
+            <div>{children}</div>
+          </div>
+          <div className="payment__pets-info">
+            <p>pets</p>
+            <div>{pets}</div>
+          </div>
+          <div className="payment__checkIn-info">
+            <p>checkIn</p>
+            <div>{checkIn}</div>
+          </div>
+          <div className="payment__checkOut-info">
+            <p>checkOut</p>
+            <div>{checkOut}</div>
+          </div>
+          <div className="payment__price-info">
+            <p>price</p>
+            <div>{price}</div>
+          </div>
+        </div>
+      </section>
+
+      <section className="payment__payment-container">
+        <form onSubmit={handleSubmit}>
+          <CardElement className="payment__payment-form" />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
     </div>
   );
 };
