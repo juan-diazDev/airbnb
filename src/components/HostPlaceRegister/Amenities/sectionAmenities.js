dd8dcc82bf8f02e35ed47aa0a4c0b9350af44ad7
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-08 00:30:58 -0500

    feat: modify amenities styles and amenitiesObj

--- src/components/HostPlaceRegister/Amenities/sectionAmenities.js
+++ src/components/HostPlaceRegister/Amenities/sectionAmenities.js
@@ -1,35 +1,44 @@
 const amenitiesObj = [
   {
     kitchen: {
+      id: 1,
       oven: {
+        id: 11,
         title: 'Oven',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/Hoven_mzvkor.png',
       },
       refrigerator: {
+        id: 12,
         title: 'Refrigerator',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/Refrigerator_lkbpqz.png',
       },
       cookingBasics: {
+        id: 13,
         title: 'Cooking basics',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/CookingBasivs_wuxrxf.png',
       },
       dishesAndSilverware: {
+        id: 14,
         title: 'Dishes and silverware',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/DishesAndSilverware_nbcz9h.png',
       },
       stove: {
+        id: 15,
         title: 'Stove',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/Stove_vmxdxx.png',
       },
       riceMaker: {
+        id: 16,
         title: 'Rice maker',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/RiceMaker_dmg0tc.png',
       },
       diningTable: {
+        id: 17,
         title: 'Dining table',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/DiningTable_po4j1x.png',
       },
       outdoorDiningArea: {
+        id: 18,
         title: 'Outdoor dining area',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/OutdoorDiningArea_fj9dsz.png',
       },
@@ -37,35 +46,44 @@ const amenitiesObj = [
   },
   {
     bathroom: {
+      id: 2,
       bodySoap: {
+        id: 21,
         title: 'Body Soap',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/Soap_fg3fxk.png',
       },
       shampoo: {
+        id: 22,
         title: 'Shampoo',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/Shampoo_vpramj.png',
       },
       toiletpaper: {
+        id: 23,
         title: 'Toilet paper',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/ToiletPaper_rxtzhr.png',
       },
     },
   },
   {
-    bedroomAndLaudry: {
+    bedroomAndLaundry: {
+      id: 3,
       towels: {
+        id: 31,
         title: 'Towels',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/Towel_zpnay6.png',
       },
       bedSheets: {
+        id: 32,
         title: 'Bed Sheets',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660008093/images/Amenities/BedSheet_jbef2i.png',
       },
       pillows: {
+        id: 33,
         title: 'Pillows',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/Pillow_xugf4j.png',
       },
       blanket: {
+        id: 34,
         title: 'Blankets',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660008093/images/Amenities/Blanket_hkfld4.png',
       },
@@ -73,19 +91,24 @@ const amenitiesObj = [
   },
   {
     facilities: {
+      id: 4,
       privetePool: {
+        id: 41,
         title: 'Private pool',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/PrivatePool_svqtcc.png',
       },
       privateHotTub: {
+        id: 42,
         titile: 'Private hot tub',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/PrivateHotTub_dgoilc.png',
       },
       houseStaff: {
+        id: 43,
         title: 'House staff',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660008437/images/Amenities/BuildingStaff_uvuuls.png',
       },
       BBQgrill: {
+        id: 44,
         title: 'BBQ grill',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/BBQGrill_kfs4yi.png',
       },
@@ -93,19 +116,24 @@ const amenitiesObj = [
   },
   {
     entertaiment: {
+      id: 5,
       wifi: {
+        id: 51,
         title: 'WiFi',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/WiFi_tpjfzo.png',
       },
       tv: {
+        id: 52,
         title: 'TV',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/TV_mp0ov9.png',
       },
       netflix: {
+        id: 53,
         title: 'Netflix',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006634/images/Amenities/Netflix_wvfgvx.png',
       },
       videoGameCosnsole: {
+        id: 54,
         title: 'Video Game Console',
         icon: 'https://res.cloudinary.com/equipo-maravilla/image/upload/v1660006635/images/Amenities/VideoGameConsole_anoidd.png',
       },
