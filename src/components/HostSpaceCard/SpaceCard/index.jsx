a121b8f6e9e47319a096bcec0d5fce6773ac5ad8
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-08 21:27:20 -0500

    feat: reorder HosSpaceCard folder

--- src/components/HostSpaceCard/SpaceCard/index.jsx
+++ src/components/HostSpaceCard/SpaceCard/index.jsx
@@ -1,12 +1,23 @@
 import PropTypes from 'prop-types';
-import './styles.scss';
+import { Link } from 'react-router-dom';
 
 const CardSpaceOffert = ({ space }) => {
-  console.log('🚀 ~ file: index.jsx ~ line 9 ~ CardSpaceOffert ~ space', space);
+  console.log('🚀 ~ file: space', space);
   return (
-    <div>
-      hola
-    </div>
+    <>
+      <Link className="space-link" to={`/SpaceDetail/${space._id}`} key={space._id}>
+        <div className="card__image-container">
+          <div>
+            <img className="card__image__image" src={space.img[0]} alt={space.title} />
+          </div>
+        </div>
+      </Link>
+      <div className="info-container">
+        <div className="card__title"><b>{space.title}</b></div>
+        <p className="card__description">{space.description}</p>
+        <div className="card__price"> ${space.price} <span>night</span></div>
+      </div>
+    </>
   );
 };
 
--- src/components/HostSpaceCard/index.jsx
+++ src/components/HostSpaceCard/index.jsx
@@ -1,24 +1,17 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { getHostSpaces } from '../../../services/spaces';
-import { loadSpaceDetail } from '../../../store/action/space';
-import { fetchUserDetail } from '../../../store/action/user';
-import Header from '../../Header';
-import CardSpaceOffert from '../SpaceCard';
+import { getHostSpaces } from '../../services/spaces';
+import { loadSpaceDetail } from '../../store/action/space';
+import { fetchUserDetail } from '../../store/action/user';
+import Header from '../Header';
+import CardSpaceOffert from './SpaceCard';
 import './styles.scss';
 
 const HostHome = () => {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const { _id } = useSelector((state) => state?.user?.userDetail);
   const spaces = useSelector((state) => state?.space?.spaceDetail);
-
-  const logOut = () => {
-    localStorage.clear();
-    navigate('/');
-  };
 
   useEffect(() => {
     dispatch(fetchUserDetail());
@@ -38,13 +31,6 @@ const HostHome = () => {
   return (
     <div>
       <Header />
-      <Link to="/PropertyTypeGroup">
-        <button type="button"> Create space</button>
-      </Link>
-      <Link to="/Loginform">
-        <button type="button"> log in</button>
-      </Link>
-      <button onClick={logOut} type="button"> log out</button>
       {
         spaces?.map((space) => (
           <CardSpaceOffert space={space} key={space._id} />
