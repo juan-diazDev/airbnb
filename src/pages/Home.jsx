601f0590338e773b31b47f728bffed92eabe73a8
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-08 21:14:44 -0500

    feat: modify rendering in home and data class in reducer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,27 @@
 /* eslint-disable react/jsx-no-useless-fragment */
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import MainSection from '../components/MainSection';
 import Footer from '../components/Footer';
-import HostHome from '../components/HostSpaceCard/HostHome';
 
 const Home = () => {
+  const navigate = useNavigate();
   const result = localStorage.getItem('profile');
   const profile = JSON.parse(result);
 
   const role = profile?.role;
+
+  useEffect(() => {
+    if (role) {
+      navigate('/HostHome');
+    }
+  }, [role]);
   return (
     <>
-      {
-        role === 'HOST'
-          ? <HostHome />
-          : (
-            <>
-              <Header />
-              <MainSection />
-              <Footer />
-            </>
-          )
-      }
+      <Header />
+      <MainSection />
+      <Footer />
     </>
   );
 };
