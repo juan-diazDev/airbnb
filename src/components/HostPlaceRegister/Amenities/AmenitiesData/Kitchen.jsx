a68db7f6cefa931c1a62d927dbb31b39c28b7354
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-09-09 05:19:37 -0500

    feat: modify Kitchen amenitie

--- src/components/HostPlaceRegister/Amenities/AmenitiesData/Kitchen.jsx
+++ src/components/HostPlaceRegister/Amenities/AmenitiesData/Kitchen.jsx
@@ -1,68 +1,77 @@
+import { useState } from 'react';
 import amenities from './amenitiesIcons';
 
 const {
   kitchen,
 } = amenities;
 
-const Kitchen = () => (
-  <section className="amenities__section">
-    <h3 className="amenities__title">{kitchen.title}</h3>
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Oven</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.oven} alt="oven" />
-      </div>
-    </article>
+const Kitchen = () => {
+  const [kitchenStaff, setKitchenStaff] = useState({});
 
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Refrigerator</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.refrigerator} alt="refrigerator" />
-      </div>
-    </article>
+  const handleClick = (e) => {
+    setKitchenStaff({ ...kitchenStaff, [e.target.name]: e.target.value });
+  };
 
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Cooking basics</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.cooking_basics} alt="cooking_basics" />
-      </div>
-    </article>
+  return (
+    <section className="amenities__section">
+      <h2 className="amenities__title">{kitchen.title}</h2>
+      <button type="button" name="Oven" value="Oven" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Oven</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.oven} alt="oven" />
+        </div>
+      </button>
 
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Dishes and silverware</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.dishes_and_silverware} alt="dishes_and_silverware" />
-      </div>
-    </article>
+      <button type="button" name="Refrigerator" value="Refrigerator" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Refrigerator</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.refrigerator} alt="refrigerator" />
+        </div>
+      </button>
 
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Stove</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.stove} alt="stove" />
-      </div>
-    </article>
+      <button type="button" name="Cooking basics" value="Cooking basics" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Cooking basics</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.cooking_basics} alt="cooking_basics" />
+        </div>
+      </button>
 
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Rice maker</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.rice_maker} alt="rice_maker" />
-      </div>
-    </article>
+      <button type="button" name="Dishes and silverware" value="Dishes and silverware" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Dishes and silverware</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.dishes_and_silverware} alt="dishes_and_silverware" />
+        </div>
+      </button>
 
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Dining table</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.dining_table} alt="dining_table" />
-      </div>
-    </article>
+      <button type="button" name="Stove" value="Stove" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Stove</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.stove} alt="stove" />
+        </div>
+      </button>
 
-    <article className="amenities__article-container">
-      <div className="amenities__article__title">Outdoor dining area</div>
-      <div className="amenities__article__image-container">
-        <img src={kitchen.oven} alt={kitchen.title} />
-      </div>
-    </article>
-  </section>
-);
+      <button type="button" name="Rice maker" value="Rice maker" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Rice maker</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.rice_maker} alt="rice_maker" />
+        </div>
+      </button>
+
+      <button type="button" name="Dining table" value="Dining table" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Dining table</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.dining_table} alt="dining_table" />
+        </div>
+      </button>
+
+      <button type="button" name="Outdoor dining area" value="Outdoor dining area" onClick={handleClick} className="amenities__article-container">
+        <div className="amenities__article__title">Outdoor dining area</div>
+        <div className="amenities__article__image-container">
+          <img src={kitchen.oven} alt={kitchen.title} />
+        </div>
+      </button>
+    </section>
+  );
+};
 
 export default Kitchen;
